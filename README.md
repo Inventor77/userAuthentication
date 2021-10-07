465d66259ebbe8bb52db9ee60b615053e7ae88bd
Shreedutt <shreedutt77@gmail.com>
2021-10-07 22:55:32 +0530

    User registeration

---
+++ README.md
@@ -0,0 +1,14 @@
+# UserAuthentication
+
+### What it does
+
+**UserAuthentication** is a web app that allows users to Authenticate themselves and add their data to App's database
+
+### How I built it
+
+
+### Built With
+
+`Express` `Nodemon` `MongoDB Mongoose` `dotenv` 
+
+
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Routes
+const authRoute = require("./routes/authRoute");
+
+dotenv.config();
+
+// DB
+mongoose.connect(
+	process.env.MONGOOSE_CONNECT,
+	{ useNewURLParser: true },
+	() => {
+		console.log("connected to db!");
+	}
+);
+
+// Middleware
+app.use(express.json());
+
+// Route Middleware
+app.use("/", authRoute);
+
+app.listen(5000, () => {
+	console.log("up and running");
+});
