43a0c4adae281c1a156663c6fc43af0dd00fe3e5
Shreedutt <shreedutt77@gmail.com>
2021-10-08 08:30:57 +0530

    register backend is completed

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -15,7 +15,7 @@ router.post("/signup", async (req, res) => {
 	});
 	try {
 		const savedUser = await user.save();
-		res.send(savedUser);
+		res.send({user: user._id});
 	} catch (err) {
 		res.status(400).send(err);
 	}
